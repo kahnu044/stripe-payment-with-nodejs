74aeba8266cd03856da54d1bf40942fc518f4123
kahnu044 <kahnucharan044@gmail.com>
2025-03-20 13:04:43 +0530

    Payment form update

--- views/card.js
+++ views/card.js
@@ -1,41 +1,17 @@
-const stripe = Stripe('pk_test_');
-const elements = stripe.elements();
+const form = document.getElementById("payment-form");
 
-const cardElement = elements.create('card');
-cardElement.mount('#card-element');
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Payment Button Click");
 
-const form = document.getElementById('payment-form');
-const errorMessage = document.getElementById('error-message');
-const successMessage = document.getElementById('success-message');
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+  const amount = parseFloat(document.getElementById("stripe-amount").value);
+  console.log("amount", amount);
 
-    const amount = parseFloat(form.querySelector('input[name="amount"]').value);
-
-    const response = await fetch('/create-payment-intent', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            amount: 12,
-            name: "kanhu",
-            email: "test@test.com"
-        }),
-    });
+  if (!amount || amount <= 9) {
+    console.log("Invalid amount, amount must be greater than 10");
+  }
 
 
-    const { clientSecret } = await response.json();
 
-    const result = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: cardElement,
-        },
-    });
 
-    if (result.error) {
-        errorMessage.textContent = result.error.message;
-    } else {
-        successMessage.textContent = 'Payment successful!';
-    }
 });
